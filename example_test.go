894bb97a479255d620ecf680acc00fa6b8c8ad0f
Justin Cormack <justin.cormack@docker.com>
2017-02-13 09:05:56 +0000

    Add an example
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

---
+++ example_test.go
@@ -0,0 +1,27 @@
+package memfd_test
+
+import (
+	"fmt"
+
+	"github.com/justincormack/go-memfd"
+)
+
+func Example() {
+	mfd, err := memfd.Create("example")
+	if err != nil {
+		panic(err)
+	}
+	defer mfd.Close()
+	_, _ = mfd.WriteString("add some contents")
+	err = mfd.SetImmutable()
+	if err != nil {
+		panic(err)
+	}
+	b, err := mfd.Map()
+	if err != nil {
+		panic(err)
+	}
+	defer mfd.Unmap()
+	fmt.Println(string(b))
+	// Output: add some contents
+}
