a20129d14a580f9fa94f5e5e946512cc11a3dbe6
Justin Cormack <justin.cormack@docker.com>
2017-02-14 23:00:12 +0000

    Add a test for a large arena that needs multiple allocations
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- memproto/arena_test.go
+++ memproto/arena_test.go
@@ -1,6 +1,8 @@
 package memproto
 
 import (
+	"os"
+	"strings"
 	"testing"
 
 	air "github.com/justincormack/go-memfd/memproto/internal/aircraftlib"
@@ -47,5 +49,49 @@ func TestArena(t *testing.T) {
 	}
 	if d.Day() != 7 {
 		t.Errorf("Incorrect day: %d", d.Day())
+	}
+}
+
+func TestLargeArena(t *testing.T) {
+	arena, err := Create()
+	if err != nil {
+		t.Errorf("failed to create memfd arena: %v", err)
+	}
+	defer arena.Close()
+
+	_, s, err := capnp.NewMessage(arena)
+	if err != nil {
+		t.Errorf("allocation error: %v", err)
+	}
+	d, err := air.NewRootPlaneBase(s)
+	if err != nil {
+		t.Errorf("root error %v", err)
+	}
+	long := strings.Repeat("fly ", os.Getpagesize())
+	d.SetName(long)
+	d.SetCanFly(false)
+
+	arena.Unmap()
+
+	err = arena.SetImmutable()
+	if err != nil {
+		t.Errorf("make Immutable failed")
+	}
+
+	msg := arena.Message()
+
+	d, err = air.ReadRootPlaneBase(msg)
+	if err != nil {
+		t.Errorf("read root error %v", err)
+	}
+	name, err := d.Name()
+	if err != nil {
+		t.Errorf("Expected a name")
+	}
+	if name != long {
+		t.Errorf("Incorrect name: %s", name)
+	}
+	if d.CanFly() != false {
+		t.Errorf("Incorrectly claims it can fly")
 	}
 }
